2685d433b4229cfbb33472be25adbe7f8dec9c1d
Matheus Sena <matheusalexandre.sena99@gmail.com>
2022-05-16 21:13:33 -0300

    improving logic behind domain contract

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -7,16 +7,31 @@ import "hardhat/console.sol";
 contract Domains {
   mapping(string => address) public domains;
 
+  // Checkout our new mapping! This will store values
+  mapping(string => string) public records;
+
   constructor() {
     console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
   }
 
   function register(string calldata name) public {
+    require(domains[name] == address(0), "this domain is not avaiable");
+    
     domains[name] = msg.sender;
     console.log("%s has registered a domain!", msg.sender);
   }
 
   function getAddress(string calldata name) public view returns (address) {
     return domains[name];
+  }
+
+  function setRecord(string calldata name, string calldata record) public {
+    // Check that the owner is the transaction sender
+    require(domains[name] == msg.sender, "this domain is not your");
+    records[name] = record;
+  }
+
+  function getRecord(string calldata name) public view returns(string memory) {
+    return records[name];
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,21 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
   console.log("Contract deployed to:", domainContract.address);
-};
+  console.log("Contract deployed by:", owner.address);
+
+  let txn = await domainContract.register("doom");
+  await txn.wait();
+
+  const domainAddress = await domainContract.getAddress("doom");
+  console.log("Owner of domain doom:", domainAddress);
+
+  // Trying to set a record that doesn't belong to me!
+  txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
+  await txn.wait();
+}
 
 const runMain = async () => {
   try {
